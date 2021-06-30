d4b70f3948804e07d8fa1410b7bf3d953739314d
ResminiLoris <resminiloris@gmail.com>
2021-06-30 16:53:26 +0200

    password_generator

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = prompt('Inserisci il tuo nome: ');
+var namePlaceholder = document.getElementById('name');
+namePlaceholder.innerHTML = name;
+
+var surname = prompt('Inserisci il tuo cognome: ');
+var surnamePlaceholder = document.getElementById('surname');
+surnamePlaceholder.innerHTML = surname;
+
+var color = prompt('Inserisci il tuo colore preferito: ');
+var colorPlaceholder = document.getElementById('color');
+colorPlaceholder.innerHTML = color;
+
+var password = name + surname + color + 21
+var passwordPlaceholder = document.getElementById('password');
+passwordPlaceholder.innerHTML = password;
